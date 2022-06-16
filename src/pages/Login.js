8549d31f09011fc5cb626abfa47079caa7dfb53f
jerson27 <jerson.27rios@gmail.com>
2022-06-15 19:41:12 -0500

    Entrar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { Component } from 'react';
+import '../components/styles/Login.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Login = () => {
-  return (
-    <div>
-      <h1>Iniciar sesion</h1>
-    </div>
-  );
-};
+class Login extends Component {
+ 
+    render() {
+        return (
+
+     
+    <div className="containerPrincipal">
+        <div className="containerSecundario">
+              
+          <div className="form-group">
+            <h2>Iniciar sesion</h2>
+            <label>Usuario: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+            />
+
+            <br />  
+            <label>Contraseña: </label>
+            <br />
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+            />
+            <br />
+            <button className="btn btn-primary" >Iniciar Sesión</button>
+          </div>
+        </div>
+      </div>
+        );
+    }
+}
 
 export default Login;
