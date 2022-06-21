0548da86026d9267b776f8fed537461a29f0649d
jcastrillong <jhonatancastrillon34@gmail.com>
2022-06-20 23:53:27 -0500

    view bills

--- src/pages/Bills.js
+++ src/pages/Bills.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { PDFViewer } from '@react-pdf/renderer'
 
 import DocumentPDF from "../components/DocumentPDF";
+import { getBills, getBill } from "../services/bill.service"
+
+import "./Bill.css"
 
 const details = [
   {
@@ -31,11 +35,58 @@ const details = [
 ]
 
 const Bills = () => {
+  const [bills, setBills] = useState([]);
+  const [bill, setBill] = useState({
+    client: {},
+    productsSold: [],
+    total: 0
+  });
+  
+  useEffect(() => {
+    getBills().then(res => {
+      console.log(res)
+      setBills(res);
+    });
+  }, []);
+  console.log(bill)
+  const handleClick = (e) => {
+    const id = e.target.id
+    getBill(id).then(res => {
+      setBill(res);
+    });
+  }
+
   return (
-    <div>
-      <PDFViewer>
-        <DocumentPDF details={details} />
-      </PDFViewer>
+    <div className="bill-content">
+      <div className="bill-subcontent">
+        <div className="list-bills">
+        {
+          bills.map((bill, key) => (
+            <div className="bill-item" key={bill.idBill} onClick={handleClick}>
+              <div className="bill-key" id={bill.idBill}>{key + 1}</div>
+              <div className="bill-id" id={bill.idBill}>{`Factura No. ${bill.idBill}`}</div>
+              <div className="bill-date" id={bill.idBill}>{bill.date.split("T")[0]}</div>
+            </div>
+          ))
+        }
+        </div>
+        <Link to="/add-bills" className="btn btn-blue">Agregar Factura</Link>
+      </div>
+      {
+        bill 
+          ? (
+            <div className="view-bill-pdf">
+              <PDFViewer style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: "10px",
+                borderColor: "#9F9F9F"
+              }}>
+                <DocumentPDF bill={bill} />
+              </PDFViewer>
+            </div>
+          ) : null
+      }
     </div>
   );
 }
