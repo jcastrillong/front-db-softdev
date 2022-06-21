23a0e8ba2879993f03c1d9587f5331c659a19599
jcastrillong <jhonatancastrillon34@gmail.com>
2022-06-20 20:31:25 -0500

    create documentPDF, clean package.json

--- src/pages/Bills.js
+++ src/pages/Bills.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { PDFViewer } from '@react-pdf/renderer'
+
+import DocumentPDF from "../components/DocumentPDF";
+
+const details = [
+  {
+    cantidad: 1,
+    descripcion: "Producto 1",
+    valor: 100000,
+    valorTotal: 100
+  },
+  {
+    cantidad: 3,
+    descripcion: "Producto 212312 123 12 312 31231 231 2321 3123",
+    valor: 300,
+    valorTotal: 900
+  },
+  {
+    cantidad: 2,
+    descripcion: "Producto 3",
+    valor: 500,
+    valorTotal: 1000
+  },
+  {
+    cantidad: 1,
+    descripcion: "Producto 4",
+    valor: 1000,
+    valorTotal: 1000
+  },
+]
 
 const Bills = () => {
   return (
-    <>
-      <h1>Bills</h1>
-    </>
+    <div>
+      <PDFViewer>
+        <DocumentPDF details={details} />
+      </PDFViewer>
+    </div>
   );
 }
 
